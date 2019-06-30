8ee02f1f1a2f026ce90a30baad56a0a2988e9c76
thiagohenriquesantos <thiagosantosengcivil@gmail.com>
2019-06-30 17:45:03 -0300

    campaign controller created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
 
   def render_404
